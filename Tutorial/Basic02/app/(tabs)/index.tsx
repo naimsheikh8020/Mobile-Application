3d143a3372f12c60a63587825166a11121a8ff16
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-01-19 05:05:26 +0600

    fix: update app icon and splash screen image to use logo

--- Tutorial/Basic02/app/(tabs)/index.tsx
+++ Tutorial/Basic02/app/(tabs)/index.tsx
@@ -1,12 +1,15 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 import React from "react";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
 
 const index = () => {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-5xl font-bold text-primary text-center">
-        Welcome this is
-      </Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0"/>
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight:"100%"}}>
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+      </ScrollView>
     </View>
   );
 };
