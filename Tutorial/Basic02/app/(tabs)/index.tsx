14042c5e73a7e077cbfc71dedbcbeafb18d93713
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-01-19 04:08:35 +0600

    feat: restructure app layout with tab navigation and remove old index component

---
+++ Tutorial/Basic02/app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const index = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-5xl font-bold text-primary text-center">
+        Welcome this is
+      </Text>
+    </View>
+  );
+};
+
+export default index;
